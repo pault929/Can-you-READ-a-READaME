68c103c64ba861448aa5905a378fabee7706efd5
Paul T <paultoro101@gmail.com>
2020-10-07 20:50:35 -0400

    done

--- index.js
+++ index.js
@@ -68,11 +68,6 @@ function generateHTML(answers) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <style>
-      body{
-          color:red;
-      }
-  </style>
 </head>
 
 <body>
